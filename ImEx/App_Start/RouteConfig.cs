438c6d7b4a7dd0475d4c652c267dc83a1419a9e0
Sokolov <sokolov_ea@farmsib.ru>
2018-07-26 12:04:52 +0300

    Add project files.

---
+++ ImEx/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ImEx
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: null,
+                url: "ImEx/F0/SendBill/{*pathInfo}",
+                defaults: new { controller = "F0", action = "SendBill" }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "ImEx/F0/Index/{*pathInfo}",
+                defaults: new { controller = "F0", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "ImEx/F0/{*pathInfo}",
+                defaults: new { controller = "F0", action = "WaitingPage" }
+            );
+        }
+    }
+}
---
+++ ImEx/Controllers/F0Controller.cs
@@ -0,0 +1,97 @@
+﻿using Nskd;
+using System;
+using System.Collections;
+using System.Data;
+using System.Web.Mvc;
+
+namespace ImEx.Controllers
+{
+    public class F0Controller : Controller
+    {
+        public Object Index()
+        {
+            Object r = "FsImEx_ImEx_HomeController_Index()";
+
+            DataSet ds = new DataSet();
+
+            RequestPackage rqp = new RequestPackage
+            {
+                Command = "ПолучитьСписокРасходныхНакладных"
+            };
+            ResponsePackage rsp = rqp.GetResponse("http://127.0.0.1:11004/"); // 1С Фарм-Сиб
+
+            if (rsp != null && rsp.Data != null && rsp.Data.Tables.Count > 0)
+            {
+                ds.Tables.Add(rsp.Data.Tables[0].Copy());
+            }
+
+            rqp.Command = "ПолучитьСписокПриходныхНакладных";
+            rsp = rqp.GetResponse("http://127.0.0.1:11014/"); // 1С ФК Гарза
+
+            if (rsp != null && rsp.Data != null && rsp.Data.Tables.Count > 0)
+            {
+                ds.Tables.Add(rsp.Data.Tables[0].Copy());
+            }
+
+            r = PartialView("~/Views/F0/Index.cshtml", ds);
+
+            return r;
+        }
+
+        public Object WaitingPage()
+        {
+            return PartialView("~/Views/F0/WaitingPage.cshtml");
+        }
+
+        public Object SendBill(String fsN)
+        {
+            String status = String.Format($"FsImEx.ImEx.Controllers.F0Controller.SendBill({fsN})");
+
+            // получить расходную накладную из 1С "Фарм-Сиб" по номеру
+            String РасходнаяНакладная = ПолучитьИз1СФармСибРасходнуюНакладную(fsN);
+            if (РасходнаяНакладная != null)
+            {
+                status += String.Format($"\nПолучена расходная накладная №{fsN}");
+
+                ДобавитьВ1СФКГарзаПриходнуюНакладную(РасходнаяНакладная);
+            }
+            return status;
+        }
+
+        private String ПолучитьИз1СФармСибРасходнуюНакладную(String fsN)
+        {
+            String РасходнаяНакладная = null;
+            RequestPackage rqp = new RequestPackage
+            {
+                Command = "ПолучитьИз1СФармСибРасходнуюНакладную",
+                Parameters = new RequestParameter[]
+                {
+                    new RequestParameter { Name = "fsN", Value = fsN }
+                }
+            };
+            ResponsePackage rsp = rqp.GetResponse("http://127.0.0.1:11004/");
+            if (rsp != null && rsp.Data != null && rsp.Data.Tables.Count > 0 && rsp.Data.Tables[0].Rows.Count > 0)
+            {
+                РасходнаяНакладная = rsp.Data.Tables[0].Rows[0][0] as String;
+            }
+            return РасходнаяНакладная;
+        }
+
+        private void ДобавитьВ1СФКГарзаПриходнуюНакладную(String РасходнаяНакладная)
+        {
+            RequestPackage rqp = new RequestPackage
+            {
+                Command = "ДобавитьВ1СФКГарзаПриходнуюНакладную",
+                Parameters = new RequestParameter[]
+                {
+                    new RequestParameter { Name = "РасходнаяНакладная", Value = РасходнаяНакладная }
+                }
+            };
+            ResponsePackage rsp = rqp.GetResponse("http://127.0.0.1:11014/");
+            if (rsp != null)
+            {
+
+            }
+        }
+    }
+}
